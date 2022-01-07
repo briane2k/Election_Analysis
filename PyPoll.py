bddd7c0820b96d3ac5886155d6128dee9b96b69b
Neags <38869150+Neags@users.noreply.github.com>
2022-01-07 15:05:06 -0800

    completed section 354

--- PyPoll.py
+++ PyPoll.py
@@ -6,6 +6,13 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+candidate_votes = {}
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -15,13 +22,28 @@ with open(file_to_load) as election_data:
 
     # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
 
 
-    
     # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # Add the candidate name to the candidate list.
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        
+        candidate_votes[candidate_name] += 1
 
 
     # To do: read and analyze the data here.
+
+for name in candidate_options:
+
+    percentage_votes = (float(candidate_votes[name]) / float(total_votes)) * 100
+
+    print(f"Candidate: {name} recieved({candidate_votes[name]:,}): {percentage_votes:2.1f}% of the total({total_votes:,}) Votes")
